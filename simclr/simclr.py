8673c034dad785d456ca9a1dd6783a355e1977b4
ChengweiZhou <131209977+ChengwZhou@users.noreply.github.com>
2023-08-20 22:14:29 -0700

    updated SimCLR-snn

--- simclr/simclr.py
+++ simclr/simclr.py
@@ -3,7 +3,7 @@ import torchvision
 
 from simclr.modules.resnet_hacks import modify_resnet_model
 from simclr.modules.identity import Identity
-
+from simclr.modules.spike_layer import *
 
 class SimCLR(nn.Module):
     """
@@ -11,9 +11,10 @@ class SimCLR(nn.Module):
     SNN-version
     """
 
-    def __init__(self, encoder, projection_dim, n_features):
+    def __init__(self, encoder, projection_dim, n_features, timestep):
         super(SimCLR, self).__init__()
 
+        self.timestep = timestep
         self.encoder = encoder
         self.n_features = n_features
 
@@ -23,14 +24,31 @@ class SimCLR(nn.Module):
         # We use a MLP with one hidden layer to obtain z_i = g(h_i) = W(2)σ(W(1)h_i) where σ is a ReLU non-linearity.
         self.projector = nn.Sequential(
             nn.Linear(self.n_features, self.n_features, bias=False),
-            nn.ReLU(),
+            # nn.ReLU(),
+            MLF_unit(self.timestep),
             nn.Linear(self.n_features, projection_dim, bias=False),
         )
 
     def forward(self, x_i, x_j):
-        h_i = self.encoder(x_i)
-        h_j = self.encoder(x_j)
+        h_i_temp = self.encoder(x_i)
+        h_j_temp = self.encoder(x_j)
 
-        z_i = self.projector(h_i)
-        z_j = self.projector(h_j)
+        timestep = self.timestep
+        b_size = h_i_temp.shape[0]
+        h_i = torch.zeros((timestep * b_size,) + h_i_temp.shape[1:], device=h_i_temp.device)
+        h_j = torch.zeros((timestep * b_size,) + h_j_temp.shape[1:], device=h_j_temp.device)
+        for t in range(timestep):
+            h_i[t*b_size:(t+1)*b_size, ...] = h_i_temp
+            h_j[t*b_size:(t+1)*b_size, ...] = h_j_temp
+
+        z_i_temp = self.projector(h_i)
+        z_j_temp = self.projector(h_j)
+
+        z_i = torch.zeros((b_size,) + z_i_temp.shape[1:], device=z_i_temp.device)
+        z_j = torch.zeros((b_size,) + z_j_temp.shape[1:], device=z_j_temp.device)
+        for t in range(self.timestep):
+            z_i += z_i_temp[t*b_size:(t+1)*b_size, ...]
+            z_j += z_j_temp[t*b_size:(t+1)*b_size, ...]
+        z_i /= self.timestep
+        z_j /= self.timestep
         return h_i, h_j, z_i, z_j
